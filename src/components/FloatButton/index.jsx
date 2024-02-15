b0e084a1aaa4de67bb05e8bfa160fc84188f3ec5
Bekzod <bekzodmahammadov627@gmail.com>
2024-02-15 11:09:41 +0500

    float button tahrirlandi

--- src/components/FloatButton/index.jsx
+++ src/components/FloatButton/index.jsx
@@ -1,28 +1,43 @@
 import {CommentOutlined, CustomerServiceOutlined} from '@ant-design/icons';
-import React from 'react';
-import {FloatButton} from 'antd';
-const Floatbutton = () => (
-    <>
-        <FloatButton.Group
-            trigger='click'
-            type='primary'
-            style={{
-                right: 24,
-            }}
-            icon={<CustomerServiceOutlined />}>
-            <FloatButton />
-            <FloatButton icon={<CommentOutlined />} />
-        </FloatButton.Group>
-        <FloatButton.Group
-            trigger='hover'
-            type='primary'
-            style={{
-                right: 94,
-            }}
-            icon={<CustomerServiceOutlined />}>
-            <FloatButton />
-            <FloatButton icon={<CommentOutlined />} />
-        </FloatButton.Group>
-    </>
-);
+import React, {useState} from 'react';
+
+import {FloatButton, Switch} from 'antd';
+
+const Floatbutton = () => {
+    const [open, setOpen] = useState(true);
+    const onChange = checked => {
+        setOpen(checked);
+    };
+    return (
+        <div>
+            <FloatButton.Group
+                trigger='hover'
+                type='primary'
+                style={{
+                    right: 84,
+                }}
+                icon={<CustomerServiceOutlined />}>
+                <FloatButton />
+                <FloatButton icon={<CommentOutlined />} />
+            </FloatButton.Group>
+            <FloatButton.Group
+                open={open}
+                trigger='click'
+                style={{
+                    right: 24,
+                }}
+                icon={<CustomerServiceOutlined />}>
+                <FloatButton />
+                <FloatButton icon={<CommentOutlined />} />
+            </FloatButton.Group>
+            <Switch
+                onChange={onChange}
+                checked={open}
+                style={{
+                    margin: 16,
+                }}
+            />
+        </div>
+    );
+};
 export default Floatbutton;
